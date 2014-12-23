f2f08647395ae6028f385f28e8c9abcb6c58609d
heavymehul <mdraja@gmail.com>
2014-12-23 14:24:32 +0000

    Update p5.ml

--- p5.ml
+++ p5.ml
@@ -1,18 +1,19 @@
 let rec gcd a b =
     if b = 0 then a
     else gcd b ( a mod b) 
-    in
+;;
 
     let rec lcm a b =
     if b = 0 then a
     else (a * b) / (gcd a b)
-    in
+;;
  
     let range n =
       let rec range n l =
       if (n<=1) then l
       else range (n-1) (n::l) in
       range n []
+;;
   
     print_int (List.fold_left (fun a b -> lcm a b) 1 (range 20))
 
