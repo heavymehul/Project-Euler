9200cc0aa7bc672a72b524037fd44bd1e5096034
heavymehul <mdraja@gmail.com>
2014-12-23 14:21:40 +0000

    Update p5.ml

--- p5.ml
+++ p5.ml
@@ -11,8 +11,8 @@ let rec gcd a b =
     let range n =
       let rec range n l =
       if (n<=1) then l
-      else range (n-1) (n::l)
-    in
+      else range (n-1) (n::l) in
+      range n []
   
     print_int (List.fold_left (fun a b -> lcm a b) 1 (range 20))
 
